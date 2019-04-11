103fd434f5e898df13cc4e5f2b85901b37699502
raph0203 <49409018+raph0203@users.noreply.github.com>
2019-04-11 11:16:53 +0000

    Delete true_false.py

--- true_false.py
+++
@@ -1,8 +0,0 @@
-
-from random import *
-r = randint(0,9)
-a = input( " veuillez entrez un nombre entre 0 et 9 : ")
-if r == a:
-    print("Félicitation, vous avez trouvé !!!")
-else:    
-    print("Dommage, mais on recommence!!!")
